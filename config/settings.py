beb49ffe7a24c78ed55d31a975da5abcd4acc57a
keenager <keenager84@gmail.com>
2023-03-09 03:08:04 +0900

    railway / csrf 관련 setting 추가

--- config/settings.py
+++ config/settings.py
@@ -135,3 +135,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+
+CSRF_TRUSTED_ORIGINS = ['https://*.web-production-8456.up.railway.app']
